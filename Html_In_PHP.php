3deaa1c8759e1ed425ee69bbc75767b7dabf7918
shubham409 <sm091274@gmail.com>
2021-12-01 13:21:10 +0530

    Html tags in php

---
+++ Html_In_PHP.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>string</h1>";
+        echo "<hr>";
+        echo "<h2>string</h2>";
+        echo "<br>";
+        echo "<h3>string</h3>";
+    ?>
+</body>
+</html>
