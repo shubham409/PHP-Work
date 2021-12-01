04e5f638187b17ff88f190446437960b4041c05e
shubham409 <sm091274@gmail.com>
2021-12-01 18:00:16 +0530

    Comparison operators

--- max_of_two_number.php
+++ max_of_two_number.php
@@ -17,6 +17,31 @@
         }
         echo getmax(10,20);
         echo "<br>";
+
+        function getmaxthree($num1,$num2,$num3){
+            if($num1>$num2){
+                if($num1>$num3){
+                    return $num1;
+                }
+                else{
+                    return $num2;
+                }
+            }   
+            else{
+                if($num2>$num3){
+                    return $num2;
+                }
+                else{
+                    return $num3;
+                }
+            }
+        }
+        echo getmaxthree(30,20,20);
+        echo "<br>";
+        echo getmaxthree(20,30,20);
+        echo "<br>";
+        echo getmaxthree(20,20,30);
+        echo "<br>";
     ?>
 </body>
 </html>
