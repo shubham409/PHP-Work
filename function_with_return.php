8096f877fc4051152a90df7f4362edf104ddd3d5
shubham409 <sm091274@gmail.com>
2021-12-01 16:59:26 +0530

    Functions and return statements

---
+++ function_with_return.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        function square($num){
+            return $num**2;
+        }
+        echo square(8);
+        echo "<br>";
+
+        $ans = square(9);
+        echo $ans;
+        echo "<br>";
+
+        
+    ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        function welcome($name){
+            echo "Welcome $name in our team";
+            echo "<br>";
+
+        }
+        welcome("shubham");
+        welcome("Jhon Doe");
+    ?>
+</body>
+</html>
