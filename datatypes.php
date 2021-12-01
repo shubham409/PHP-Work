c43bdde6b2d214f5505265cd7a94679423c9d037
shubham409 <sm091274@gmail.com>
2021-12-01 13:35:30 +0530

    Basic datatypes in php

---
+++ datatypes.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $string_variable = "Plain text inside";
+        $integer_variable = 80;
+        $float_variable = 80.00001;
+        $boolean_variable = TRUE;
+        $null_variable = NULL;
+
+        echo $string_variable;
+        echo "<br>";
+        echo $integer_variable;
+        echo "<br>";
+        echo $float_variable;
+        echo "<br>";
+        echo $boolean_variable;
+        echo "<br>";
+        echo $null_variable;
+        echo "<br>";
+    ?>
+</body>
+</html>
