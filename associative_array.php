e65abc44ae8a498ce415815abf188af1ad3a3304
shubham409 <sm091274@gmail.com>
2021-12-01 16:47:08 +0530

    Associative array

---
+++ associative_array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        // Storing Key value pair in associative array(with unique keys)
+        $dict = array(
+            "name" => "shubham",
+            "course" =>"computer science"
+        );
+
+        echo $dict["name"];
+        echo "<br>";
+        
+        echo $dict["course"];
+        echo "<br>";
+    ?>
+</body>
+</html>
