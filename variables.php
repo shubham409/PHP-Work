a95d4e325515ccfd33bf1f0ac6747cfee8ece411
shubham409 <sm091274@gmail.com>
2021-12-01 13:28:17 +0530

    variables in php

---
+++ variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $number = 70;
+        $string ="string";
+        $boolean = TRUE;
+        $nullvalue = NULL;
+        echo $number;
+        echo "<br>";
+
+        echo $string;
+        echo "<br>";
+
+        echo $boolean;
+        echo "<br>";
+            
+        echo $nullvalue;
+        echo "<br>";     
+        
+        
+    ?>
+</body>
+</html>
