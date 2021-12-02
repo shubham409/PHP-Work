52d7adf04ae361d0f1be7604de380f499718a730
shubham409 <sm091274@gmail.com>
2021-12-02 12:35:13 +0530

    Getters And Setters

---
+++ inheritance.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        echo gettype(gettype(1));
+        echo "<br>";
+            
+
+    ?>
+
+</body>
+</html>
