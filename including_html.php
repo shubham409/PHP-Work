dbf0007e48efa5bfa5cecb92fd797c17712caa3c
shubham409 <sm091274@gmail.com>
2021-12-02 10:39:39 +0530

    Including html Files

---
+++ including_html.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+        include "header.html";
+        include "header.html";
+
+    ?>
+    <?php
+        include "footer.html";
+    ?>
+</body>
+</html>
